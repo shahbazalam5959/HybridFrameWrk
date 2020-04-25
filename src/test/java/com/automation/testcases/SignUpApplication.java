db1ee727652edf64f92d2b5d036d63622beafaaa
shahbazalam5959 <shahbazalam5959@gmail.com>
2020-04-26 00:17:31 +0530

    Revert "Basic Commands Sysout"
    This reverts commit 63c26097a5c6d02d577c030d01dae0d70a4c093f.

--- src/test/java/com/automation/testcases/SignUpApplication.java
+++ src/test/java/com/automation/testcases/SignUpApplication.java
@@ -5,9 +5,6 @@ public class SignUpApplication {
 	private void syso() {
 		// TODO Auto-generated method stub
 		System.out.println("Learning basic commands of GIT");
-		
-		System.out.println("Learning Restore,Revert commands of GIT");
-		
 	}
 	
 }
