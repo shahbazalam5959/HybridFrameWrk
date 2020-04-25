1693f9255175be0d05540b28dc48da69073c3b5c
shahbazalam5959 <shahbazalam5959@gmail.com>
2020-04-25 23:56:59 +0530

    Adding Syso method

--- src/test/java/com/automation/testcases/SignUpApplication.java
+++ src/test/java/com/automation/testcases/SignUpApplication.java
@@ -2,4 +2,9 @@ package com.automation.testcases;
 
 public class SignUpApplication {
 
+	private void syso() {
+		// TODO Auto-generated method stub
+		System.out.println("Learning basic commands of GIT");
+	}
+	
 }
