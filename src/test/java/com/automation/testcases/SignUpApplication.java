ad5b3ef4d3e134bc7aeb1840c38267152dff6340
shahbazalam5959 <shahbazalam5959@gmail.com>
2020-04-26 12:44:05 +0530

    Revert "Added New line in SignUp.java"
    This reverts commit 61e4e0543407a27f24e3b3ef3a6aa55c0ac3c48d.

--- src/test/java/com/automation/testcases/SignUpApplication.java
+++ src/test/java/com/automation/testcases/SignUpApplication.java
@@ -3,11 +3,8 @@ package com.automation.testcases;
 public class SignUpApplication {
 
 	private void syso() {
-		
+		// TODO Auto-generated method stub
 		System.out.println("Learning basic commands of GIT");
-		
-		System.out.println("Learning basic commands of GIT bramches");
-		
 	}
 	
 }
