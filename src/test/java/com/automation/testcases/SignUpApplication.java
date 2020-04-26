82923b0c73a68f518a11646483a099e6aabe4a73
shahbazalam5959 <shahbazalam5959@gmail.com>
2020-04-26 13:04:16 +0530

    Added sum method

--- src/test/java/com/automation/testcases/SignUpApplication.java
+++ src/test/java/com/automation/testcases/SignUpApplication.java
@@ -8,6 +8,14 @@ public class SignUpApplication {
 		
 		System.out.println("Learning basic commands of GIT bramches");
 		
+		sum(100,200);
+		
 	}
 	
+	public void sum(int x,int y)
+	{
+		int a=x,b=y;
+		System.out.println((a+b));
+		
+	}
 }
