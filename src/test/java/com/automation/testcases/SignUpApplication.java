864355692153ddf75c29f403db11104edf4b25dd
shahbazalam5959 <shahbazalam5959@gmail.com>
2020-04-26 12:47:21 +0530

    Syso Line added

--- src/test/java/com/automation/testcases/SignUpApplication.java
+++ src/test/java/com/automation/testcases/SignUpApplication.java
@@ -3,8 +3,11 @@ package com.automation.testcases;
 public class SignUpApplication {
 
 	private void syso() {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Learning basic commands of GIT");
+		
+		System.out.println("Learning basic commands of GIT bramches");
+		
 	}
 	
 }
